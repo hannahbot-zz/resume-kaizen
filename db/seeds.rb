947b37281331c6115782762c15ba88f29ce34bce
Hannah Bottalla <hannah@instructure.com>
2014-04-16 19:35:03 -0500

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,7 @@
 require 'faker'
 
 u = User.new(
-  name: 'Admin User',
-  email: 'admin@example.com',
-  password: 'password',
-  password_confirmation: 'password')
-u.skip_confirmation!
-u.save
-u.update_attribute(:role, 'admin')
-
-u = User.new(
-  name: 'Reviewer',
+  name: 'Reviewer 1',
   email: 'reviwer@example.com',
   password: 'password',
   password_confirmation: 'password')
@@ -19,7 +10,43 @@ u.save
 u.update_attribute(:role, 'reviewer')
 
 u = User.new(
-  name: 'Submitter',
+  name: 'Reviewer 2',
+  email: 'reviwer@example.com',
+  password: 'password',
+  password_confirmation: 'password')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'reviewer')
+
+u = User.new(
+  name: 'Reviewer 3',
+  email: 'reviwer@example.com',
+  password: 'password',
+  password_confirmation: 'password')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'reviewer')
+
+u = User.new(
+  name: 'Submitter 1',
+  email: 'submitter@example.com',
+  password: 'password',
+  password_confirmation: 'password')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'submitter')
+
+u = User.new(
+  name: 'Submitter 2',
+  email: 'submitter@example.com',
+  password: 'password',
+  password_confirmation: 'password')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'submitter')
+
+u = User.new(
+  name: 'Submitter 3',
   email: 'submitter@example.com',
   password: 'password',
   password_confirmation: 'password')
